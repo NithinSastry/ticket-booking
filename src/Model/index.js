4240a10441581d8f17be77831b05e09ab9fbf908
NithinSastry <nithin_Sastry@outlook.com>
2020-08-27 16:55:04 +0530

    basic event system.

---
+++ src/Model/index.js
@@ -0,0 +1,8 @@
+//maintains the state of the application.
+//invokes service broker API to store data into local storage
+
+class TicketModel {
+  constructor() {}
+}
+
+export default TicketModel;
--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import Tickets from '../data';
 import { setTickets } from './Utils/service-broker';
+import TicketModel from './Model/index';
 
 //set the initial ticket data in the local storage
 console.log(Tickets);
 setTickets(Tickets);
+
+//initialise the model
+const ticketModel = new TicketModel();

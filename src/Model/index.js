15c08be2c2e80492e4297081027b79a07094ef7d
NithinSastry <nithin_Sastry@outlook.com>
2020-08-27 17:36:41 +0530

    basic model methods.

--- src/Model/index.js
+++ src/Model/index.js
@@ -9,7 +9,17 @@ class TicketModel {
     this.tickets = getTickets();
     this.eventHub = getEventHub();
     this.eventHub.publish(EVENTS.DATA_LOADED, this.tickets);
+    this.eventHub.subscribe(EVENTS.TICKET_SELECTED, this.onTicketSelect);
   }
+  onTicketSelect = (category, rowId, seatNo) => {
+    if (category && rowId) {
+      const row = this.tickets[category][rowId];
+      row[seatNo] = true;
+      this.eventHub.publish(EVENTS.RE_RENDER_ROW, {
+        rowData: row,
+      });
+    }
+  };
 }
 
 export default TicketModel;
